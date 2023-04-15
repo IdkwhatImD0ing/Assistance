0e5e14353af6a1fb5cc8e0f9fef68a75360c3136
Bill Zhang <billzhangsc@gmail.com>
2023-04-15 12:19:11 -0700

    Added props

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,8 +2,9 @@
 import * as React from "react"
 import { Box, Typography, TextField, InputAdornment } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
+import AppContext from '../appContext'
 
-const QuestionField = () => {
+const QuestionField = (props) => {
   return (
     <TextField
       id="outlined-basic"
@@ -11,6 +12,8 @@ const QuestionField = () => {
       variant="outlined"
       multiline
       fullWidth
+      onChange={props.onChange}
+      onSubmit={props.onSubmit}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
@@ -22,43 +25,50 @@ const QuestionField = () => {
     />
   )
 }
-import AppContext from '../appContext'
+
 
 const IndexPage = () => {
   const {resetSharedData} = useContext(AppContext)
+  const [text, setText] = useState('')
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const pageLoadedAt = window.performance.timing.navigationStart
       const timeSincePageLoad = Date.now() - pageLoadedAt
 
       if (timeSincePageLoad < 100) {
-        clearCookies()
         resetSharedData
         navigate('/')
       }
     }
   }, [])
 
+  const submit = () => {}
+
   return (
     <Box
-    width = "100w"
-    height = "100vh"
-    display = "flex"
-    flexDirection="column"
-    justifyContent = "center"
-    alignItems = "center"
-  >
-    <Box
-      width = "70vw"
-      height = "100vh"
-      display = "flex"
+      width="100w"
+      height="100vh"
+      display="flex"
       flexDirection="column"
-      justifyContent = "center"
-      alignItems = "center"
+      justifyContent="center"
+      alignItems="center"
     >
-      <Typography variant="h1" component="h2" gutterBottom>Assistance</Typography>
-      <QuestionField/>
-    </Box>
+      <Box
+        width="70vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h1" component="h2" gutterBottom>
+          Assistance
+        </Typography>
+        <QuestionField
+          onChange={(e) => setText(e.target.value)}
+          onSubmit={submit}
+        />
+      </Box>
     </Box>
   )
 }
