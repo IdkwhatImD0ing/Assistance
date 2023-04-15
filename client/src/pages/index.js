5b83c660a306323378bc5a386caede40e54fe427
Yatrik <ypatel2@ucsc.edu>
2023-04-15 12:07:29 -0700

    set up basic frontend

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,36 @@
 
 import * as React from "react"
-import { Box, Typography, TextField } from "@mui/material"
+import { Box, Typography, TextField, InputAdornment } from "@mui/material"
+import SendIcon from '@mui/icons-material/Send';
+
+const QuestionField = () => {
+  return (
+    <TextField id="outlined-basic" label="Ask a question" variant="outlined" 
+    multiline
+    fullWidth
+    InputProps={{
+      endAdornment: (
+        <InputAdornment position="end">
+          <SendIcon/>
+        </InputAdornment>
+      ) 
+    }}
+    color="secondary"/>
+  )
+}
 
 const IndexPage = () => {
   return (
     <Box
-      width = "100vw"
+    width = "100w"
+    height = "100vh"
+    display = "flex"
+    flexDirection="column"
+    justifyContent = "center"
+    alignItems = "center"
+  >
+    <Box
+      width = "70vw"
       height = "100vh"
       display = "flex"
       flexDirection="column"
@@ -13,7 +38,8 @@ const IndexPage = () => {
       alignItems = "center"
     >
       <Typography variant="h1" component="h2" gutterBottom>Assistance</Typography>
-      <TextField id="outlined-basic" label="Ask a question" variant="outlined" />
+      <QuestionField/>
+    </Box>
     </Box>
   )
 }
