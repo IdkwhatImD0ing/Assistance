a179ecfdc9195a3842fcf4be54aa6d0c469c11ff
Bill Zhang <billzhangsc@gmail.com>
2023-04-15 11:03:13 -0700

    Conversations work

--- server/main.py
+++ server/main.py
@@ -2,35 +2,45 @@ from os import environ
 from fastapi import FastAPI
 import asyncio
 from pydantic import BaseModel
+from typing import List
 from EdgeGPT import Chatbot, ConversationStyle
 from Bard import Chatbot as BardChatbot
 from dotenv import load_dotenv
 load_dotenv()
 
-
 app = FastAPI()
 token = environ.get("BARD_TOKEN")
 bard_chatbot = BardChatbot(token)
 
-class Prompt(BaseModel):
-    text: str
+class Message(BaseModel):
+    role: str
+    message: str
+
+class Conversation(BaseModel):
+    conversation: List[Message]
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/bingchat")
-async def bingchat(prompt: Prompt):
-    async def ask_chatbot(prompt_text):
-        bot = Chatbot(cookiePath = environ.get("BING_PATH"))
-        response = await bot.ask(prompt=prompt_text, conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub")
-        await bot.close()
-        return response
+async def ask_chatbot(conversation):
+    bot = Chatbot(cookiePath=environ.get("BING_PATH"))
+    conversation_text = "\n".join([f"{msg.role}: {msg.message}" for msg in conversation])
+    response = await bot.ask(prompt=conversation_text, conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub")
+    await bot.close()
+    return response
 
-    response = await ask_chatbot(prompt.text)
+@app.post("/bingchat")
+async def bingchat(conversation: Conversation):
+    response = await ask_chatbot(conversation.conversation)
     return {"response": response["item"]["messages"][-1]["text"]}
 
+def ask_bard_chatbot(conversation):
+    conversation_text = "\n".join([f"{msg.role}: {msg.message}" for msg in conversation])
+    response = bard_chatbot.ask(conversation_text)
+    return response
+
 @app.post("/bardchat")
-def bardchat(prompt: Prompt):
-    response = bard_chatbot.ask(prompt.text)
+def bardchat(conversation: Conversation):
+    response = ask_bard_chatbot(conversation.conversation)
     return {"response": response["content"]}
