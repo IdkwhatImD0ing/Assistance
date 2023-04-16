10ff4360b9b269efcd3ccbf513823a1a27b63dac
Bill Zhang <billzhangsc@gmail.com>
2023-04-15 19:44:59 -0700

    Changes

--- client/src/pages/previousSession.js
+++ client/src/pages/previousSession.js
@@ -1,40 +1,33 @@
 import React from "react";
-import { Card, Typography, Stack, Box} from "@mui/material";
+import { Typography, Stack, Box} from "@mui/material";
 import BotResponse from "../components/botResponse";
 
 const PreviousQuestionSession = (props) => {
   return (
-    <Box
-    width="100vw"
-    height="100vh">
-        <Box         display="flex"
+    <Box width="100vw" height="100vh">
+      <Box
+        display="flex"
         flexDirection="column"
         justifyContent="space-between"
         alignItems="center"
         spacing={2}
         overflow={'auto'}
->
-          <Typography variant="h2">Previous Sessions</Typography>
-          <Stack
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={2}
-            width="50%"
-          >
-              <BotResponse
-                name="Say my name."
-                text="Previous session 1"
-                avatar={require('../images/bingLogo.png').default}
-              />
-              <BotResponse
-                name="I am inevitable."
-                text="Previous session 2"
-                avatar={require('../images/bardLogo.png').default}
-              />
-          </Stack>
-        </Box>
-{/* 
+      >
+        <Typography variant="h2">Previous Sessions</Typography>
+        <Stack
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={2}
+          width="50%"
+        >
+          <BotResponse
+            name="I am inevitable."
+            text="Previous session 2"
+          />
+        </Stack>
+      </Box>
+      {/* 
 <Stack
       sx={{
         width: "100%",
@@ -50,9 +43,8 @@ const PreviousQuestionSession = (props) => {
       </Card>
       <BotResponse name="Session 1" text="Previous Session"></BotResponse>
     </Stack> */}
-  </Box>
-    
-  );
+    </Box>
+  )
 };
 
 export default PreviousQuestionSession;
